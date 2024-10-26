d3ac44eb7755ea6151a393a3f7b00b4fd3067a2d
Ilya Sovtsov <ilyasovty@gmail.com>
2024-10-26 14:47:05 +0700

    Fix (?)

--- solution/task3.sh
+++ solution/task3.sh
@@ -2,6 +2,4 @@
 
 # Вывести дату в формате:
 #   13 октября 2023 года, Пятница
-LC_ALL=
-LC_TIME=ru_RU.utf8
-date '+%-d %B %Y года, %A' -d "$@"
+LC_ALL=ru_RU.UTF-8 LC_TIME=ru_RU.UTF-8 date '+%-d %B %Y года, %A' -d "$@"
